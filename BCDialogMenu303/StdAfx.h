9e0a86a438f5fd2836b722b0e4c9f1d11278792b
Valentin Kroupkin <assotut@gmail.com>
2015-11-08 21:13:46 +1300

    BCDialogMenu: fix _WIN32_WINNT undefined warning

--- BCDialogMenu303/StdAfx.h
+++ BCDialogMenu303/StdAfx.h
@@ -10,6 +10,17 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
+
+#include <winsdkver.h>
+
+#ifndef _WIN32_WINNT
+// Modify the following define to target a prior platform. Refer to MSDN
+// for the latest info on corresponding values for different platforms.
+#define _WIN32_WINNT _WIN32_WINNT_MAXVER
+#endif
+
+#include <sdkddkver.h>
+
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>         // MFC core and standard components
