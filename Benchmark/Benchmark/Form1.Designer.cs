f0f32f0eb18b06f1168bdbd0cf70dc7880861fe1
morphine43 <morphine1934@gmail.com>
2020-05-27 05:29:43 +0300

    add benchmark sources

---
+++ Benchmark/Benchmark/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "Form1";
+        }
+        private void InitializeComponent(uint mode, uint itemsCount, uint rowCount = 1, int seed = 0, int length = 8)
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "Form " + itemsCount.ToString();
+            uint colCount = itemsCount / rowCount;
+            if (itemsCount % rowCount != 0)
+            {
+                colCount++;
+            }
+
+            Random r = new Random(seed);
+            string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            for (uint i = 0; i < itemsCount; i++)
+            {
+                StringBuilder text = new StringBuilder(length);
+                for (int i2 = 0; i2 < length; i2++)
+                {
+                    text.Append(characters[r.Next(characters.Length)]);
+                }
+                var label = new Label();
+                label.Text = text.ToString();
+
+                text = new StringBuilder(length);
+                for (int i2 = 0; i2 < length; i2++)
+                {
+                    text.Append(characters[r.Next(characters.Length)]);
+                }
+
+                Control obj = new Control();
+
+                switch (mode)
+                {
+                    case 1:
+                        var btn = new Button();
+                        btn.Text = text.ToString();
+                        obj = btn;
+                        break;
+                    case 2:
+                        var txt = new TextBox();
+                        txt.Text = text.ToString();
+                        obj = txt;
+                        break;
+                    case 3:
+                        var combo = new ComboBox();
+                        combo.Text = text.ToString();
+                        text = new StringBuilder(length);
+                        for (int i2 = 0; i2 < length; i2++)
+                        {
+                            text.Append(characters[r.Next(characters.Length)]);
+                        }
+                        combo.Items.Add(text.ToString());
+                        text = new StringBuilder(length);
+                        for (int i2 = 0; i2 < length; i2++)
+                        {
+                            text.Append(characters[r.Next(characters.Length)]);
+                        }
+                        combo.Items.Add(text.ToString());
+                        text = new StringBuilder(length);
+                        for (int i2 = 0; i2 < length; i2++)
+                        {
+                            text.Append(characters[r.Next(characters.Length)]);
+                        }
+                        combo.Items.Add(text.ToString());
+                        obj = combo;
+                        break;
+                }
+
+
+                int height = label.Height;
+                if (height < obj.Height)
+                {
+                    height = obj.Height;
+                }
+                int width = label.Width + obj.Width;
+
+                label.Top = (int)(i % colCount) * height;
+                label.Left = (int)(i / colCount) * width;
+                obj.Top = label.Top;
+                obj.Left = label.Right;
+                this.Controls.Add(label);
+                this.Controls.Add(obj);
+            }
+
+            this.AutoScroll = true;
+        }
+        #endregion
+    }
+}
+
---
+++ Benchmark/Benchmark/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static int Main(string[] args)
+        {
+            // Test if input arguments were supplied:
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a numeric arguments.");
+
+                return 1;
+            }
+
+            try
+            {
+                // Convert the input arguments to numbers:
+                uint mode = uint.Parse(args[0]);
+                uint itemsCount = uint.Parse(args[1]);
+                uint rowCount = uint.Parse(args[2]);
+
+
+                if (itemsCount <= 0)
+                {
+                    System.Console.WriteLine("Please enter a positive numeric arguments.");
+
+                    return 1;
+                }
+
+                if (rowCount <= 0)
+                {
+                    System.Console.WriteLine("Please enter a positive numeric arguments.");
+
+                    return 1;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1(mode, itemsCount, rowCount));
+
+                return 0;
+            }
+            catch (System.FormatException)
+            {
+                System.Console.WriteLine("Please enter a numeric arguments.");
+
+                return 1;
+            }
+        }
+    }
+}
